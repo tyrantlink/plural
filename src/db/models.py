ad0792401c7dfa7ae8a2a1da45d5862b70d97d4d
tyrantlink <38902185+tyrantlink@users.noreply.github.com>
2024-10-13 22:41:04 -0700

    updated proxy tag validation

--- src/db/models.py
+++ src/db/models.py
@@ -1,11 +1,28 @@
+from pydantic import BaseModel, Field, model_validator, ValidationError
 from beanie import PydanticObjectId
-from pydantic import BaseModel, Field
 
 
 class ProxyTag(BaseModel):
-    prefix: str
-    suffix: str
-    regex: bool
+    prefix: str = Field(
+        '',
+        max_length=10,
+        description='tag must have a prefix or suffix')
+    suffix: str = Field(
+        '',
+        max_length=10,
+        description='tag must have a prefix or suffix')
+    regex: bool = False
+
+    @model_validator(mode='after')
+    def check_prefix_and_suffix(cls, values):
+        prefix = values.get('prefix', '')
+        suffix = values.get('suffix', '')
+
+        if not prefix and not suffix:
+            raise ValidationError(
+                'At least one of prefix or suffix must be non-empty')
+
+        return values
 
 
 class DatalessImage(BaseModel):
