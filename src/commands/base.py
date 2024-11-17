96801f96a5073912ef4f08e70cf3a6f9781e42e4
TyrantLink <tyrantlink@pm.me>
2024-11-17 15:16:49 -0800

    reimplement import help embed

--- src/commands/base.py
+++ src/commands/base.py
@@ -454,7 +454,29 @@ async def slash_import(
     file_url: str | None = None
 ) -> None:
     if file is None and file_url is None:
-        raise InteractionError('help embed not implemented')
+        # ? zero-width spaces to stop discord stupid list formatting looking like shit in embeds
+        embed = Embed(
+            title='how to import data',
+            color=0x69ff69)
+        embed.add_field(
+            name='pluralkit',
+            value='\n'.join([
+                '​1. start a DM with pluralkit',
+                '​2. send `pk;export` and copy the link it DMs you',
+                '​3. use the `/import` command and paste the link to the `file_url` parameter']),
+            inline=False)
+        embed.add_field(
+            name='tupperbox',
+            value='\n'.join([
+                '​1. start a DM with tupperbox',
+                '​2. send `tul!export` and copy the link it DMs you',
+                '​3. use the `/import` command and paste the link to the `file_url` parameter']),
+            inline=False)
+
+        await interaction.response.send_message(
+            embeds=[embed]
+        )
+        return
 
     url = file.url if file else file_url
     assert url is not None
