35e50f350a3c963a6c934394d20f42556bc28ff5
tyrantlink <38902185+tyrantlink@users.noreply.github.com>
2025-01-11 12:51:26 -0800

    add outline for bot, api, and egress-proxy

---
+++ bot/lib/bot.ex
@@ -0,0 +1,18 @@
+defmodule Bot do
+  @moduledoc """
+  Documentation for `Bot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ bot/mix.exs
@@ -0,0 +1,28 @@
+defmodule Bot.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bot,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:redix, "~> 1.5"},
+      {:mongodb_driver, "~> 1.5.0"}
+    ]
+  end
+end
